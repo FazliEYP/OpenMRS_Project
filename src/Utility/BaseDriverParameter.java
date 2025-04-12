4e73efb6a623904afe0dcd8886685fbbf5e3f467
FAZLI <fazlieyupo@gmail.com>
2025-04-12 21:25:00 +0300

    Proje Olusturuldu

---
+++ src/Utility/BaseDriverParameter.java
@@ -0,0 +1,44 @@
+package Utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+
+public class BaseDriverParameter {
+    // aşağısını SDET8 den aldık
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @BeforeClass
+    @Parameters("BrowserTipi")
+    public void Setup(String browserTipi)
+    {
+// seleniumdaki static yani başlangıç kısımlar konacak
+        switch (browserTipi.toLowerCase()){
+            case "firefox": driver=new FirefoxDriver(); break;
+            case "edge": driver=new EdgeDriver(); break;
+            default : driver=new ChromeDriver();
+        }
+
+        driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30)); // 20 sn mühlet: sayfayı yükleme mühlet
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // 5 sn mühlet: elementi bulma mühleti
+
+        wait=new WebDriverWait(driver, Duration.ofSeconds(20));
+
+
+    }
+
+
+
+
+
+
+
+}
