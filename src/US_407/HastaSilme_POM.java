88b909520d3f7149a42c415efc126b9d7c518906
muratoksuz <murattoksuz@gmail.com>
2025-04-19 14:54:09 +0300

    Patient delete test cooked

---
+++ src/US_407/HastaSilme_POM.java
@@ -0,0 +1,43 @@
+package US_407;
+
+import Utility.BaseDriver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class HastaSilme_POM extends BaseDriver {
+@Test
+    public void HastaSilme(){
+
+    WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(20));
+    HastaSilme_Elements hse = new HastaSilme_Elements();
+    driver.get("https://o2.openmrs.org/openmrs/login.htm");
+
+
+    wait1.until(ExpectedConditions.visibilityOf(hse.userName));
+    hse.userName.sendKeys("admin");
+    hse.password.sendKeys("Admin123");
+    hse.regDesk.click();
+    hse.loginButton.click();
+
+    wait1.until(ExpectedConditions.elementToBeClickable(hse.findPatientRecord));
+    hse.findPatientRecord.click();
+
+    wait1.until(ExpectedConditions.visibilityOf(hse.searchBox));
+    hse.searchBox.sendKeys("100KFD" + Keys.ENTER);
+    hse.activePatient.click();
+
+
+    wait1.until(ExpectedConditions.elementToBeClickable(hse.deletePatientLink));
+    hse.deletePatientLink.click();
+
+    hse.reasonBox.sendKeys("Patient demand");
+    wait1.until(ExpectedConditions.elementToBeClickable(hse.reasonConfirm));
+    hse.reasonConfirm.click();
+
+
+}
+}
